33cd924f0db50b73c33da9f258c3f0e6386eede3
debsouryadatta <debsouryadatta@gmail.com>
2023-06-10 23:59:26 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+
+import connectDB from './mongodb/connect.js'
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
+dotenv.config() // To get the access of dotenv files
+
+const app = express()
+app.use(cors())
+app.use(express.json({limit: '50mb'})) // For getting the req.body
+
+app.use('/api/v1/post', postRoutes) // Providing the base path to the routes
+app.use('/api/v1/dalle', dalleRoutes) // Providing the base path to the routes
+
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-E')
+})
+
+const startServer = async (req, res) => {
+    try {
+        connectDB(process.env.MONGODB_URL)
+        app.listen(8080, ()=> console.log('Server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+startServer()
